71c9fee9d8093f2be2ba2af09bd00c5e96318e16
bonxbons <144151209+bonxbons@users.noreply.github.com>
2024-03-18 14:58:39 -0500

    0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+# 0-gather_data_from_an_API.py
+import sys
+import requests
+
+def get_employee_todo_progress(employee_id):
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        done_tasks = [task for task in data if task['completed']]
+        total_tasks = len(data)
+
+        employee_name = data[0]['username'].capitalize()
+        number_of_done_tasks = len(done_tasks)
+
+        print(f'Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):')
+
+        for task in done_tasks:
+            print(f'\t{task["title"]}')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 0-gather_data_from_an_API.py <employee_id>')
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
