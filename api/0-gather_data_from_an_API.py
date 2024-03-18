b5e5a4aa6e67804b9db8b3ed6a8f166c1ffbc5c8
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-18 15:48:45 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,7 +16,7 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()
     user_name = user_data['name']
 
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    todos_url = (f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
     todos_response = requests.get(todos_url)
 
     if todos_response.status_code != 200:
@@ -26,17 +26,4 @@ def get_employee_todo_progress(employee_id):
     total_tasks = len(todos_response.json())
     completed_tasks = sum(task['completed'] for task in todos_response.json())
 
-    print(f'Employee {user_name} is done with tasks({completed_tasks}/{total_tasks}):')
-
-    completed_tasks_titles = [task['title'] for task in todos_response.json() if task['completed']]
-
-    for task_title in completed_tasks_titles:
-        print(f'\t{task_title}')
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python3 0-gather_data_from_an_API.py <employee_id>')
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    print(f'Employee {user_name} is done with tasks({
