95abeac530e8e0faa8f7b07d4d15ef99b0f551cb
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-07-16 12:47:23 -0500

    adding functions

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -14,3 +14,22 @@ def get_tasks():
     url = "https://jsonplaceholder.typicode.com/todos"
     response = requests.get(url)
     todos = response.json()
+
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+    users = response.json()
+
+    tasks = {}
+    for user in users:
+        user_id = user["id"]
+        tasks[user_id] = []
+        for todo in todos:
+            if todo["userId"] == user_id:
+                task = {
+                    "username": user["username"],
+                    "task": todo["title"],
+                    "completed": todo["completed"]
+                }
+                tasks[user_id].append(task)
+
+    return tasks
