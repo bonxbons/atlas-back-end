a906068d7000bd802fba23cbfa6f5526d378c30a
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-07-16 01:09:17 -0500

    adding on

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,3 +8,13 @@ if __name__ == "__main__":
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
+    employee_id = int(sys.argv[1])
+
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    todos = response.json()
+
+    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    employee_response = requests.get(employee_url)
+    employee = employee_response.json()
+
